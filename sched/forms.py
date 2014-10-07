887a85e28c9c2cb41ea488bdaf9c59ea643267bc
Felipe de Jesus Miramontes Romero <felipemiramontesr@gmail.com>
2014-10-06 19:45:32 -0700

    Class Appoiments, forms.py

--- sched/forms.py
+++ sched/forms.py
@@ -1,3 +1,11 @@
 from wtforms import Form, BooleanField, DateTimeField
 from wtforms import TextAreaField, TextField
 from wtforms.validators import Length, required
+
+class AppointmentForm(Form):
+	title = TextField('Title', [Length(max=255)])
+	start = DateTimeField('Start', [required()])
+	end = DateTimeField('End')	
+	allday = BooleanField('All Day')
+	location = TextField('Location', [Length(max=255)])	
+	description = TextAreaField('Description')
