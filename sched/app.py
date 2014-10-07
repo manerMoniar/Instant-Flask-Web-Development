2339ccf1f0d77e9855c839f4221bc5987a3c03c7
Maner Moniar <maner.moniar@gmail.com>
2014-10-06 23:09:37 -0500

    Creando y mostrando registro

--- sched/app.py
+++ sched/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import abort, jsonify, redirect, render_template, request, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
-from sched.models import Base
+
+from sched.forms import AppointmentForm
+from sched.models import Base, Appointment
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
@@ -33,11 +36,13 @@ def appointment_list():
 
 @app.route('/appointments/<int:appointment_id>/')
 def appointment_detail(appointment_id):
-    """
-    >>> appointment_detail(3)
-    'Detail of appointment #3.'
-    """
-    return 'Detail of appointment #{}.'.format(appointment_id)
+    """Provide HTML page with a given appointment."""
+    # Query: get Appointment object by ID.
+    appt = db.session.query(Appointment).get(appointment_id)
+    if appt is None:
+        # Abort with Not Found.
+        abort(404)
+    return render_template('appointment/detail.html', appt=appt)
 
 
 @app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
@@ -51,11 +56,17 @@ def appointment_edit(appointment_id):
 
 @app.route('/appointments/create/', methods=['GET', 'POST'])
 def appointment_create():
-    """
-    >>> appointment_create()
-    'Form to create a new appointment.'
-    """
-    return 'Form to create a new appointment.'
+    """Provide HTML form to create a new appointment."""
+    form = AppointmentForm(request.form)
+    if request.method == 'POST' and form.validate():
+        appt = Appointment()
+        form.populate_obj(appt)
+        db.session.add(appt)
+        db.session.commit()
+        # Success. Send user back to full appointment list.
+        return redirect(url_for('appointment_list'))
+    # Either first load or validation error at this point.
+    return render_template('appointment/edit.html', form=form)
 
 
 @app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
