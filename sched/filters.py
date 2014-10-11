eb1a1120713df6061b94ae1b84ee51453e06649e
Felipe de Jesus Miramontes Romero <felipemiramontesr@gmail.com>
2014-10-11 09:23:39 -0700

    Modificaciones de unit_test y filters

--- sched/filters.py
+++ sched/filters.py
@@ -1,3 +1,7 @@
+"""Custom filters for Jinja templating. Load with init_app function."""
+
+from jinja2 import Markup, evalcontextfilter, escape
+
 def do_datetime(dt, format=None):
 	"""Jinja template filter to format a datetime object."""
 	if dt is None:
@@ -22,5 +26,108 @@ def do_datetime(dt, format=None):
 	return formatted
 
 def init_app(app):
-	"""Initialize a Flask application with custom filters."""
-	app.jinja_env.filters['datetime'] = do_datetime
+	"""Initialize a Flask application with filters defined in this module."""
+    app.jinja_env.filters['date'] = do_date
+    app.jinja_env.filters['datetime'] = do_datetime
+    app.jinja_env.filters['duration'] = do_duration
+
+    # The nl2br filter uses the Jinja environment's context to determine
+    # whether to autoescape
+    app.jinja_env.filters['nl2br'] = evalcontextfilter(do_nl2br)
+
+def do_date(dt, format='%Y-%m-%d - %A'):
+    """
+    Jinja template filter to format a datetime object with date only.
+    >>> do_date(None)
+    ''
+    >>> from datetime import datetime
+    >>> do_date(datetime(2010, 11, 11, 13, 00, 00))
+    '2010-11-11 - Thursday'
+    """
+    if dt is None:
+        return ''
+    # Only difference with do_datetime is the default format, but that is
+    # convenient enough to warrant its own template filter.
+    return dt.strftime(format)
+
+def do_duration(seconds):
+    """
+    Jinja template filter to format seconds to humanized duration.
+    >>> do_duration(3600)
+    '0 day, 1 hour, 0 minute, 0 second'
+    >>> do_duration(258732)
+    '2 days, 23 hours, 52 minutes, 12 seconds'
+    """
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    tokens = []
+    tokens.append(formatoDia(d))
+    tokens.append(formatoHora(h))
+    tokens.append(formatoMinuto(m))
+    tokens.append(formatoSegundo(s))
+
+    template = ', '.join(tokens)
+    return template.format(d=d, h=h, m=m, s=s)
+
+
+def formatoDia(d):
+    """
+    Format for days
+    >>> formatoDia(1)
+    '{d} day'
+    >>> formatoDia(2)
+    '{d} days'
+    """
+    return '{d} days' if d > 1 else '{d} day'
+
+
+def formatoHora(h):
+    """
+    Format for hour
+    >>> formatoHora(1)
+    '{h} hour'
+    >>> formatoHora(2)
+    '{h} hours'
+    """
+    return '{h} hours' if h > 1 else '{h} hour'
+
+
+def formatoMinuto(m):
+    """
+    Format for minutes
+    >>> formatoMinuto(1)
+    '{m} minute'
+    >>> formatoMinuto(30)
+    '{m} minutes'
+    """
+    return '{m} minutes' if m > 1 else '{m} minute'
+
+
+def formatoSegundo(s):
+    """
+    Format for seconds
+    >>> formatoSegundo(1)
+    '{s} second'
+    >>> formatoSegundo(23)
+    '{s} seconds'
+    """
+    return '{s} seconds' if s > 1 else '{s} second'
+
+
+def do_nl2br(context, value):
+    """
+    Render newline \n characters as HTML line breaks <br />.
+
+    By default, HTML normalizes all whitespace on display. This filter allows
+    text with line breaks entered into a textarea input to later display in
+    HTML with line breaks.
+
+    The context argument is Jinja's state for template rendering, which
+    includes configuration. This filter inspects the context to determine
+    whether to auto-escape content, e.g. convert <script> to &lt;script&gt;.
+    """
+    formatted = u'<br />'.join(escape(value).split('\n'))
+    if context.autoescape:
+        formatted = Markup(formatted)
+    return formatted
--- unit_test.py
+++ unit_test.py
@@ -112,6 +112,74 @@ class ModelsTest(unittest.TestCase):
         self.assertEqual('<Appointment: 23>', app.__repr__())
         self.assertNotEqual('<Appointment: 22>', app.__repr__())
 
+class test_filter(unittest.TestCase):
+    
+    def test_datetime_without_hour(self):
+        now = date(2010, 11, 11)
+        fecha = filters.do_datetime(now)
+        self.assertNotEqual(fecha, "2010-11-11 - Thursday")
+
+    def test_datetime_with_hour(self):
+        now = datetime(2010, 11, 11, 13, 00, 00)
+        fecha = filters.do_datetime(now)
+        self.assertEqual(fecha, '2010-11-11 - Thursday at 1:00pm')
+
+    def test_datetime_None(self):
+        fecha = filters.do_datetime(None)
+        self.assertNotEqual(fecha, "Today")
+        self.assertEqual(fecha, '')
+
+
+    def test_datetime_format_None(self):
+        now = datetime(2010, 11, 11, 14, 00, 00)
+        fecha = filters.do_datetime(now, None)
+        self.assertEqual(fecha, '2010-11-11 - Thursday at 2:00pm')
+
+    def test_datetime_with_format(self):
+        a = '%Y-%m-%d - %A'
+        now = datetime(2010, 11, 11, 14, 00, 00)
+        fecha = filters.do_datetime(now, a)
+        self.assertNotEqual(fecha, '2010-11-11 - Thursday at 2:00pm')
+
+    def test_date_none(self):
+        fechadate = filters.do_date(None)
+        self.assertEqual(fechadate, '')
+
+    def test_date_not_none(self):
+        now = datetime(2010, 11, 11, 13, 00, 00)
+        fechadate = filters.do_date(now)
+        self.assertNotEqual(fechadate, '2010-11-11 - Thursday at 1:00pm')
+        self.assertEqual(fechadate, '2010-11-11 - Thursday')
+
+    def test_duration_hour(self):
+        time = filters.do_duration(3600)
+        self.assertNotEqual(time, "1 day")
+        self.assertEqual(time, "0 day, 1 hour, 0 minute, 0 second")
+
+    def test_duration_days(self):
+        time = filters.do_duration(258732)
+        self.assertEqual(time, "2 days, 23 hours, 52 minutes, 12 seconds")
+
+
+    def test_do_nl2br_without_Markup(self):
+        template_env = Environment(
+          autoescape=False,
+         extensions=['jinja2.ext.i18n', 'jinja2.ext.autoescape'])
+        text = "Texto con '\n' para saltos '\n' pero junto"
+        changes = filters.do_nl2br(template_env, text)
+        self.assertNotEqual(changes, "")
+        self.assertEqual(changes, "Texto con &#39;<br />&#39; para saltos &#39;<br />&#39; pero junto")
+
+    def test_do_nl2br_with_Markup(self):
+        template_env = Environment(
+          autoescape=True,
+         extensions=['jinja2.ext.i18n', 'jinja2.ext.autoescape'])
+        text = "Texto con '\n' para saltos '\n' pero <script>junto</script>"
+        changes = filters.do_nl2br(template_env, text)
+        self.assertNotEqual(changes, "Texto con &#39;<br />&#39; para saltos &#39;<br />&#39; pero junto")
+        self.assertEqual(changes, "Texto con &#39;<br />&#39; para saltos &#39;<br />&#39; pero &lt;script&gt;junto&lt;/script&gt;")
+  
+
 
 if __name__ == '__main__':
     unittest.main()
